c2810c66c956e23c27b0f7fd8cb0a58530ec9e3c
Bjork Weenk <bjorkweenk.bw@gmail.com>
2022-06-13 21:01:35 +0200

    profiel

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1> Hello world </h1>
+        </nav>
+      );
+}
+ 
+export default Navbar;
---
+++ src/components/Splash/Splash.jsx
@@ -0,0 +1,23 @@
+import "./Splash.css"
+import {Carousel} from "bootstrap";
+
+
+const Splash = () => {
+    return ( 
+        <>
+        <div>
+        <div className= "Square">
+        <div className="FullName">
+        <h1 className="Name"><span className="WU"> WU  </span> QINGGUI BJORK WEENK </h1>
+       </div>
+        <div className="line"/>
+        </div>
+        <div className="verticalLine"/>
+  
+</div>
+     
+        </>
+     );
+}
+ 
+export default Splash;
